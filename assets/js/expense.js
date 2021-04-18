3d57a4425c58db52118f3af2ab2f17b1803c00b9
Sanchit <sanchit02@outlook.com>
2021-04-18 17:43:26 +0530

    updated code with javascript

--- assets/js/expense.js
+++ assets/js/expense.js
@@ -1,77 +1,101 @@
-var table = document.getElementById("table");
+let username = localStorage.getItem("Username");
+let amount = localStorage.getItem("Amount");
 
-let balance = 0;
+console.log(username, amount);
 
+let uname = document.getElementById("uname");
+let balance = document.getElementById("balance");
+
+uname.innerHTML = username;
+balance.innerHTML = "₹" + amount;
+
+// Add income
 const addIncome = (ev) => {
   ev.preventDefault();
-  let expenseMoney = parseInt(
-    document.getElementById("balance").innerHTML.slice(3)
-  );
-  let amountMoney = parseInt(
-    document.getElementById("income").innerHTML.slice(3)
-  );
-  let amount = document.getElementById("amount").value;
+  // let expenseMoney = parseInt(
+  //   document.getElementById("expense").innerHTML.slice(1)
+  // );
 
-  console.log(amount);
-  console.log(amountMoney);
-  
+  // let amountMoney = parseInt(
+  //   document.getElementById("balance").innerHTML.slice(1)
+  // );
+
+  let amount = document.getElementById("transaction-amount").value;
+
+  // console.log(amount);
+  // console.log(balance);
+
   if (amount > 10000000) {
-    document.getElementById("amount").value = "Amount should be within 1CR";
+    alert("Amount should be within 1CR");
   } else {
-    let text = document.getElementById("info").value;
+    console.log("object");
+    let text = document.getElementById("transaction-text").value;
+    let table = document.getElementById("table");
+
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerText = text;
+    cell2.innerText = amount;
+    cell3.innerHTML = `credit`;
+
+    let x = parseInt(balance.innerHTML.slice(1));
+
+    x = x + parseInt(amount);
+    balance.innerHTML = "₹" + x;
+  }
+};
+
+// Calculate the expense
+const reduceExpense = (ev) => {
+  ev.preventDefault();
+
+  let expenseMoney = parseInt(
+    document.getElementById("expense").innerHTML.slice(1)
+  );
+
+  // taking balance
+  let x = parseInt(balance.innerHTML.slice(1));
+  // let amountMoney = parseInt(
+  //   document.getElementById("balance").innerHTML.slice(3)
+  // );
+
+  let amount = document.getElementById("transaction-amount").value;
+
+  let text = document.getElementById("transaction-text").value;
+
+  if (amount > x) {
+    alert("Insufficient Balance");
+    document.getElementById("transaction-amount").style.color = "red";
+
+    setInterval(() => {
+      document.getElementById("transaction-amount").style.color = "black";
+    }, 3000);
+  } else if (x >= amount) {
     var row = table.insertRow(table.rows.length);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     cell1.innerText = text;
     cell2.innerText = amount;
-    cell3.innerHTML=`
-    <i class="fa fa-arrow-up" aria-hidden="true"></i>
-    
-    `;
-    amountMoney = amountMoney + parseInt(amount);
-    document.getElementById("income").innerHTML = "Rs " + amountMoney;
-  }
-};
+    cell3.innerHTML = `debit`;
 
-const reduceExpense = (ev) => {
-  ev.preventDefault();
-  
-  let expenseMoney = parseInt(
-    document.getElementById("balance").innerHTML.slice(3)
-  );
-  
-  let amountMoney = parseInt(
-    document.getElementById("income").innerHTML.slice(3)
-  );
-  
-  let amount = document.getElementById("amount").value;
-  
-  let text = document.getElementById("info").value;
-  
-  if (amount > amountMoney) {
-    document.getElementById("amount").value = "Insufficient Balance";
-    document.getElementById("amount").style.color = "red";
-    setInterval(() => {
-      document.getElementById("amount").style.color = "black";
-    }, 3000);
-  } else if (amountMoney >= amount) {
-    var row = table.insertRow(table.rows.length);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerText = text;
-    cell2.innerText = amount;
     expenseMoney = expenseMoney + parseInt(amount);
-    amountMoney = amountMoney - parseInt(amount);
-    document.getElementById("balance").innerHTML = "Rs " + expenseMoney;
-    document.getElementById("income").innerHTML = "Rs " + amountMoney;
+    x = x - parseInt(amount);
+
+    document.getElementById("expense").innerHTML = "₹" + expenseMoney;
+    balance.innerHTML = "₹" + x;
   }
   console.log(expenseMoney);
 };
 
+const credit = document.getElementById("credit");
+const debit = document.getElementById("debit");
+
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("btn").addEventListener("click", addIncome);
+  credit.addEventListener("click", addIncome);
 });
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("btn1").addEventListener("click", reduceExpense);
+  debit.addEventListener("click", reduceExpense);
 });
