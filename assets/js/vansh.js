77b600dfbeea996643a95e91c0bdafeefa63794e
Sanchit <sanchit02@outlook.com>
2021-04-18 15:31:43 +0530

    fixed popup

--- assets/js/vansh.js
+++ assets/js/vansh.js
@@ -1,13 +1,40 @@
-// let name=document.querySelector("#name");
-// let balance=document.querySelector("#balance");
+// let inputName = document.getElementById("input-name").innerHTML;
+// let balance = document.getElementById("input-amount").innerHTML;
+// const btn = document.getElementById("btn");
 
-function validateForm() {
-    let name=document.querySelector("#name");
-    let balance=document.querySelector("#balance");
-    if (name == "") {
-      alert("Name must be filled out");
-    }
-    else if (balance == ""){
-        alert("Balance must be filled out");
-    }
+// console.log("hello");
+
+// const events = (e) => {
+//   e.preventDefault();
+//   console.log(inputName, balance);
+//   if (inputName == "") {
+//     alert("Name must be filled out");
+//   } else if (balance == "") {
+//     alert("Balance must be filled out");
+//   }
+// };
+
+const validateForm = (e) => {
+  // e.preventDefault();
+  let inputName = document.getElementById("input-name").value;
+  let balance = document.getElementById("input-amount").value;
+  const btn = document.getElementById("btn");
+
+  // console.log(inputName);
+  // console.log(balance);
+
+  if (inputName == "") {
+    alert("Name must be filled out");
+  } else if (balance == "") {
+    alert("Balance must be filled out");
+  } else {
+    localStorage.setItem("Username", inputName);
+    localStorage.setItem("Amount", balance);
+
+    location.href = "../popupLogin/index.html"
   }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btn").addEventListener("click", validateForm);
+});
